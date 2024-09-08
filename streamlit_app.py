cccca6b51ab74bc2755236b7c9f1615908496eff
YacineOud <yacine.oudmine@gmail.com>
2024-09-08 21:32:40 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -235,7 +235,7 @@ elif page == pages[2]:
                           xaxis=dict(title='Variables'),
                           yaxis=dict(title='Variables'),
                           width=5000,
-                          height=800)
+                          height=700)
 
 # Affichage du graphique avec Streamlit
     st.plotly_chart(matrix_corr,use_container_width = True)
