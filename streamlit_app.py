78508981937586dc10795c55efd9059dd63a2d95
YacineOud <yacine.oudmine@gmail.com>
2024-09-08 21:31:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -234,8 +234,8 @@ elif page == pages[2]:
     matrix_corr.update_layout(title='Matrice de corrélation des salaires',
                           xaxis=dict(title='Variables'),
                           yaxis=dict(title='Variables'),
-                          width=1800,
-                          height=600)
+                          width=3000,
+                          height=700)
 
 # Affichage du graphique avec Streamlit
     st.plotly_chart(matrix_corr,use_container_width = True)
